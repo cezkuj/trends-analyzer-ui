05cb375d03d1e090808a361c4014d3f04f4f92c2
cezkuj <cezkuj@gmail.com>
2018-08-27 12:14:22 +0000

    Finished communication frontend <-> backend, graph is visible

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 import { Charts, ChartContainer, ChartRow, YAxis, LineChart } from "react-timeseries-charts";
 import { TimeSeries } from "pondjs";
 
-
-var pointsJSON = require('./data.json')
-var points = pointsJSON.map(point => [new Date(point.timestamp).valueOf(), point.reaction_avg])
-const data = {
-    name: "sentiment",
-    columns: ["time", "value"],
-    points: points
-};
-console.log(data)
-
-const series = new TimeSeries(data);
-
 class App extends Component {
+
+  constructor(props){
+      super(props);
+      this.setSeries = this.setSeries.bind(this);
+      this.getSeries = this.getSeries.bind(this);
+      this.state = {series: this.getSeries([[new Date().valueOf(), 0]])}
+    
+  }
+  componentDidMount(){
+      this.setSeries()
+  }
+  getSeries(points){
+      var data = {
+          name: "sentiment",
+          columns: ["time", "value"],
+          points: points
+      };
+      var series = new TimeSeries(data);
+      return series;
+  }
+
+  setSeries() {
+      axios.get('/api/analyzes?name=trump')
+           .then(res => {
+               var points = res.data.map(point => [new Date(point.timestamp).valueOf(), point.reaction_avg]);
+               this.setState({series: this.getSeries(points)});
+
+           })
+           .catch(error => {
+               console.log(error);
+           });
+
+  }
   render() {
      return (
-        <ChartContainer timeRange={series.timerange()}>
+        <ChartContainer timeRange={this.state.series.timerange()}>
             <ChartRow >
                 <YAxis id="axis1" label="trump" min={-0.3} max={0.3} />
                 <Charts>
-                    <LineChart axis="axis1" series={series} column={["trump"]}/>
+                    <LineChart axis="axis1" series={this.state.series} column={["trump"]}/>
                 </Charts>
             </ChartRow>
         </ChartContainer>
