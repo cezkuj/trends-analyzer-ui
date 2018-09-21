8178ecc8ddd45f10f50d91c01f78959ec74b632b
cezkuj <cezkuj@gmail.com>
2018-09-21 00:04:40 +0000

    Adding menu option

---
+++ src/Routes.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+
+import App from "./App";
+import Login from "./Login";
+import Register from "./Register";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Menu />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <PrivateRoute path="/dashboard" component={App} />
+        </div>
+      </Router>
+    );
+  }
+}
+class Menu extends React.Component {
+  render() {
+    if (false) {
+      return <Redirect to="/dashboard" />;
+    }
+    return (
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+    );
+  }
+}
+export default Routes;
