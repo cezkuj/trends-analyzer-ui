e94206855f1d67a845ecd690def8bcf00ff57251
cezkuj <cezkuj@gmail.com>
2018-09-23 11:59:49 +0000

    Improving logging and registring workflow

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.state = { username: "", password: "" };
+  }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value }); //Dynamic keys https://stackoverflow.com/questions/29280445/reactjs-setstate-with-a-dynamic-key-name
+  }
+  handleLogin(event) {
+    /*axios.post('/api/' + event.target.name, {
+        "username": this.state.username,
+        "password": this.state.password,
+      }, {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    */
+    this.props.history.push("/dashboard");
+  }
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        username:
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        password:
+        <input
+          type="text"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <button name="login" onClick={this.handleLogin}>
+          Login
+        </button>
+        <Link to="/register">Register</Link>
+      </form>
+    );
   }
 }
 export default Login;
--- src/Register.js
+++ src/Register.js
@@ -1,8 +1,72 @@
 import React from "react";
 
 class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
+    this.state = {
+      username: "",
+      password: "",
+      email: "",
+      registration_code: ""
+    };
+  }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value }); //Dynamic keys https://stackoverflow.com/questions/29280445/reactjs-setstate-with-a-dynamic-key-name
+  }
+  handleRegister(event) {
+    /*axios.post('/api/' + event.target.name, {
+        "username": this.state.username,
+        "password": this.state.password,
+      }, {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    */
+    this.props.history.push("/login");
+  }
   render() {
-    return <div>Register</div>;
+    return (
+      <form>
+        username:
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <br />
+        password:
+        <input
+          type="text"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <br />
+        email:
+        <input
+          type="text"
+          name="email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <br />
+        registration code:
+        <input
+          type="text"
+          name="registration_code"
+          value={this.state.registration_code}
+          onChange={this.handleChange}
+        />
+        <br />
+        <button name="register" onClick={this.handleRegister}>
+          Register
+        </button>
+      </form>
+    );
   }
 }
 export default Register;
